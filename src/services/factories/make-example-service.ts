cde6df69c872a4f6d0941a913f548225e55c6e27
salesmiguelz <miguelsales02@gmail.com>
2024-11-30 19:59:24 -0300

    feat: Adds creat pet service factory

--- src/services/factories/make-example-service.ts
+++
@@ -1,9 +0,0 @@
-// import { PrismaExamplesRepository } from "@/repositories/prisma/prisma-examples-repository";
-import { ExampleService } from "../example.service";
-
-export function makeExampleService() {
-  const exaplesRepository = new PrismaExamplesRepository();
-  const exampleService = new ExampleService(exaplesRepository);
-
-  return exampleService;
-}
