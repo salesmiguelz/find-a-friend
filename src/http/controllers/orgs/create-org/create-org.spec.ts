099e2b6859f4495faa9a54c807afe5be59fc632f
salesmiguelz <miguelsales02@gmail.com>
2024-12-02 20:34:59 -0300

    feat: Setup e2e tests and adds create org e2e test

--- src/http/controllers/orgs/create-org/create-org.spec.ts
+++ src/http/controllers/orgs/create-org/create-org.spec.ts
@@ -1,3 +1,31 @@
-import { test } from "vitest";
+import { app } from "@/app";
+import { prisma } from "@/lib/prisma";
+import createAndAuthenticateOrg from "@/utils/tests/create-and-authenticate-org";
+import request from "supertest";
+import { it, beforeAll, describe, afterAll, expect } from "vitest";
 
-test("ok", () => {});
+describe("Create Org Controller E2E", () => {
+    beforeAll(async () => {
+        await app.ready()
+    })
+
+    afterAll(async () => {
+        await app.close()
+    })
+
+    it("should be able to create an org", async () => {
+        const { token } = await createAndAuthenticateOrg(app);
+        const orgData = {
+            name: "New Beautiful Org",
+            email: "beautiful.org@gmail.com",
+            password: "123456",
+            managerName: "Miguel Sales",
+            phone: "40028922",
+            city_id: 1,
+            state_id: 1
+        }
+        const orgResponse = await request(app.server).post('/orgs').set('Authorization', `Bearer ${token}`).send(orgData)
+
+        expect(orgResponse.statusCode).toBe(201)
+    })
+})
